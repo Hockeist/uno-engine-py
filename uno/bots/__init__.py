8e3829788a25bbd2ecd00161e1cd68ae9879074f
Davbych <skripkinm17@gmail.com>
2025-11-05 14:12:11 +0700

    Add files via upload

--- uno/bots/__init__.py
+++ uno/bots/__init__.py
@@ -1,5 +1,6 @@
 from .random_bot import RandomBot
 from .wild_first_bot import WildFirstBot
 from .wild_last_bot import WildLastBot
+from .bot_by_skripkin import BotDavby
 
-__all__ = ["RandomBot", "WildFirstBot", "WildLastBot"]
+__all__ = ["RandomBot", "WildFirstBot", "WildLastBot", "BotDavby"]
