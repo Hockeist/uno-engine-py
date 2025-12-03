6fbc2077b25be608623c846a7b115d182cb0dbb3
Vansoooo <ivan.zhadnov.07@mail.ru>
2025-12-03 13:18:32 +0700

    Update __init__.py

--- uno/bots/__init__.py
+++ uno/bots/__init__.py
@@ -1,5 +1,6 @@
 from .random_bot import RandomBot
 from .wild_first_bot import WildFirstBot
 from .wild_last_bot import WildLastBot
+from .super_bot import super_bot
 
-__all__ = ["RandomBot", "WildFirstBot", "WildLastBot"]
+__all__ = ["RandomBot", "WildFirstBot", "WildLastBot", "super_bot"]
